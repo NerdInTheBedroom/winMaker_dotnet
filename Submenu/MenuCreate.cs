ae6a13359db04567a9a8a778b5fbdf17f80830cc
A1ЭX <alextronic85yt@gmail.com>
2025-03-21 13:18:15 +0700

    # Fix the create project system

--- Submenu/MenuCreate.cs
+++ Submenu/MenuCreate.cs
@@ -46,30 +46,37 @@ namespace winMaker_dotnet.Sub_menu
                 // Set project name
                 string ProjectName;
                 ProjectName = BoxName.Text;
-                // Set project path
-                string ProjectPath;
-                ProjectPath = $"{BoxDirectory.Text}/{ProjectName}";
 
-                // If project directory isn't empty and exists
-                if (Directory.Exists(BoxDirectory.Text) && ProjectName != string.Empty)
+                // If a directory with the same name as the project name already exist
+                if (Directory.Exists($"{BoxDirectory.Text}/{ProjectName}"))
                 {
+                    MessageBox.Show("A directory with this project name already exist.", "Project Name", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                // If the path(project directory location) doesn't exist
+                else if (!Directory.Exists(BoxDirectory.Text) || ProjectName == string.Empty)
+                {
+                    MessageBox.Show("Choosen directory is empty or doesn't exist.", "Project Directory", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error);
+                }
+                // If project name isn't empty and the path(project directory location) exists
+                else
+                {
+                    // Set project path
+                    string ProjectPath;
+                    ProjectPath = $"{BoxDirectory.Text}/{ProjectName}";
+
                     // Create the project directory
                     Directory.CreateDirectory(ProjectPath);
 
-                    // Create a directory for the project files inside the directory
+                    // Create a directory for the project files inside the project directory
                     Directory.CreateDirectory($"{ProjectPath}/src");
                     // Create the project files
                     File.Create($"{ProjectPath}/src/{ProjectName}.wmnodes"); // Nodes file
                     File.Create($"{ProjectPath}/src/{ProjectName}.wmcodes"); // Codes file
 
                     // Create the project loader
-                    StreamWriter ProjectLoader = new StreamWriter($"{ProjectPath}/{ProjectName}.wmp");
+                    StreamWriter ProjectLoader = new($"{ProjectPath}/{ProjectName}.wmp");
                     ProjectLoader.Write($"{ProjectPath}/src/{ProjectName}.wmnodes\n{ProjectPath}/src/{ProjectName}.wmcodes"); // Nodes, Codes
                     ProjectLoader.Close();
-                }
-                else
-                {
-                    MessageBox.Show("Choosen directory is empty or doesn't exist.", "Project Directory", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error);
                 }
             }
             else
