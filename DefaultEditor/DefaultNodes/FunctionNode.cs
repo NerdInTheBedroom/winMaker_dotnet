d27b9277eb3fbd9c1ade02a505666592cdc8847e
A1ЭX <alextronic85yt@gmail.com>
2025-03-23 16:13:09 +0700

    + Starting a function node system

---
+++ DefaultEditor/DefaultNodes/FunctionNode.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * Created on: 03/23/2025
+ * Last modified on: 03/23/2025
+ * Author: A1EX
+ * GitHub: https://github.com/GeekInTheBedroom
+ */
+
+using System.ComponentModel;
+
+namespace winMaker_dotnet.DefaultEditor.DefaultNodes
+{
+    public partial class FunctionNode : Component
+    {
+        public FunctionNode()
+        {
+            InitializeComponent();
+        }
+
+        public FunctionNode(IContainer container)
+        {
+            container.Add(this);
+
+            InitializeComponent();
+        }
+    }
+}
